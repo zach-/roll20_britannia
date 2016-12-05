02bc3af3b773b51a8e9195039541e98a0b405372
Zach <zach@wildfigdata.com>
2016-12-05 00:13:47 -0500

    github will hate me

--- image_processing/image_knit.py
+++ image_processing/image_knit.py
@@ -1,48 +1,54 @@
 import json
 from PIL import Image, ImageTk
 from cv2 import imread, imwrite
-from numpy import zeros
+from numpy import zeros, unique
 from tkinter import Toplevel, Label
 from glob import glob
 import os
+from re import search
 
 
-def test():
-    win = Toplevel()
-    path = r'C:\Users\zbric\Code\Python\roll20_britannia\tileset'
-    COLUMNS = 10
-    image_count = 0
-    for infile in glob(os.path.join(path, '*.jpg')):
-        image_count += 1
-        r, c = divmod(image_count - 1, COLUMNS)
-        im = Image.open(infile)
-        tkimage = ImageTk.PhotoImage(im)
-        myvar = Label(win, image=tkimage)
-        myvar.image = tkimage
-        myvar.grid(row=r, column=c, padx=0, pady=0)
-    win.mainloop()
+def change_values(data):
+    removex = data[0]["X"]
+    removey = data[0]["Y"]
+    for cell in data:
+        cell["X"] = cell["X"] - removex
+        cell["Y"] = cell["Y"] - removey
 
 
 def main():
+    file_list = glob('../map_json/*.txt')
     dir = '../tileset/tile_'
     ext = '.jpg'
-    tuple_list = []
-    new_img = Image.new('RGB', (4960, 3968))
-    with open('../map_json/WriteMap10.txt') as json_data:
-        data = json.load(json_data)
-
-    for x in xrange(len(data)):
-        tupl = (data[x]["CoordX"], data[x]["CoordY"], data[x]["ID"])
-        tuple_list.append(tupl)
-
-    for t in xrange(len(tuple_list) / 1024):
-        img = Image.open(dir + str(tuple_list[t][2]) + ext)
-        for x in xrange(0, 4960, 31):
-            for y in xrange(0, 3968, 31):
-                print(t)
-                new_img.paste(img, (x, y))
-
-    new_img.show()
+    coorx = 9920
+    coory = 15872
+    for file in file_list:
+        num = search('\d+', file)
+        new_img = Image.new('RGB', (coorx, coory))
+        with open(file) as json_data:
+            data = json.load(json_data)
+        change_values(data)
+        for cell in xrange(len(data)):
+            try:
+                img = Image.open(dir + str(data[cell]["ID"]) + ext)
+        # img = Image.open(dir + cell["ID"] + ext)
+                new_img.paste(img, (data[cell]["X"] * 31,
+                                    data[cell]["Y"] * 31))
+        # new_img.paste(img, (cell["X"], cell["Y"]))
+            except IOError as err:
+                print err
+            filename = './map_json/world_file_' + str(num.group(0)) + '.jpg'
+            new_img.save(filename)
+    # for x in xrange(len(data)):
+    #     tupl = (data[x]["ID"])
+    #     tuple_list.append(tupl)
+    #
+    # for y in xrange(0, coory, 31):
+    #     for x in xrange(0, coorx, 31):
+    #
+    #         new_img.paste(img, (x, y))
+    #
+    # new_img.show()
 
     # print tuple_list
 # test()
